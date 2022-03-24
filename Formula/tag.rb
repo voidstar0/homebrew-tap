86a035c89be43dbfba0a37780b0e208a207130bb
char <25884226+char@users.noreply.github.com>
2022-03-24 16:51:25 -0400

    Change hash

--- Formula/tag.rb
+++ Formula/tag.rb
@@ -2,7 +2,7 @@ class Tag < Formula
     desc "CLI for creating tagged paths and finding them"
     homepage "https://github.com/char/tag"
     url "https://github.com/char/tag/archive/refs/tags/v0.1.0.tar.gz"
-    sha256 "b4566359c1425ed6631a039e902f4467c9a9de31"
+    sha256 "69b83a2643d811bf33972ea26e2247c3631ff6d8df39ae4eca777978319956ec"
     version "0.1.0"
   
     def install
