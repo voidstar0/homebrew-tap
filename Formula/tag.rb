e9e3cf314ceb41eb2dec43f88e5efdc5b1a3de96
char <25884226+char@users.noreply.github.com>
2022-03-24 19:13:15 -0400

    Update checksum

--- Formula/tag.rb
+++ Formula/tag.rb
@@ -2,7 +2,7 @@ class Tag < Formula
     desc "CLI for creating tagged paths and finding them"
     homepage "https://github.com/char/tag"
     url "https://github.com/char/tag/releases/download/v0.2.0/tag-0.2.0-aarch_64-apple-darwin.tar.gz"
-    sha256 "a05a1e7bcacbb9a0ed4fc1d7b2d9ac743aead466"
+    sha256 "1bcd027999dba9e22661f2f90e5fccb7bd14e8587090ef72fb45664f324e0fd1"
     version "0.2.0"
   
     def install
